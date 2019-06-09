4c3b5fd71f7c8ab3c78fbef7a042dd02a37aec6d
day1park <daypark2408@gmail.com>
2019-06-10 00:46:43 +1000

    add updateContact functionality

--- Redux/contactmanager_redux/src/actions/contactActions.js
+++ Redux/contactmanager_redux/src/actions/contactActions.js
@@ -48,7 +48,7 @@ export const addContact = contact => async dispatch => {
 
 export const updateContact = contact => async dispatch => {
   const res = await axios.put(
-    "https://jsonplaceholder.typicode.com/users",
+    `https://jsonplaceholder.typicode.com/users/${contact.id}`,
     contact
   );
   dispatch({
