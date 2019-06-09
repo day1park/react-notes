2921c6b86ebfe693139535a439a2d5ff3cdcb722
day1park <daypark2408@gmail.com>
2019-06-09 18:05:11 +1000

    create initial state, middleware, and store variables

--- Redux/contactmanager_redux/src/store.js
+++ Redux/contactmanager_redux/src/store.js
@@ -1,3 +1,21 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
+
+// createstore method takes initialstate as a parameter, but don't want anything inside by default
+const initalState = {};
+
+// set to array of middleware we want to use which is thunk
+const middleware = [thunk];
+
+//
+const store = createStore(
+  rootReducer,
+  initalState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+export default store;
